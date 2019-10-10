9226a47ef10ddd690a4b16d0c830848980f4b26e
Jonathan Weiss <jonathanweiss@Jonathans-MacBook-Air.local>
2019-10-10 15:28:14 +0300

    chore: prints to movie creation process

--- Movie.py
+++ Movie.py
@@ -8,13 +8,23 @@ import os
 
 # Construct the argument parser and parse the arguments
 # TODO bug : colour seems off in the image.
+
+def print_dict(dictionary):
+    max_length = len(max(dictionary, key=len))
+    for key in dictionary:
+        print(f"{key}:{' ' * (max_length - len(key))} {dictionary[key]}")
+
+
 def get_params():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-ext", "--extension", required=False, default='jpg', help="extension name. "
-                                                                               "default is 'jpg'.")
-    ap.add_argument("-o", "--output", required=False, default='outputcheck.mp4',
-                    help="output video file")
+    ap.add_argument('-ext', '--extension', required=False, default='jpg', help='extension name. '
+                                                                               'default is "jpg".')
+    ap.add_argument('-o', '--output', required=False, default='outputcheck.mp4',
+                    help='output video file')
+
     args = vars(ap.parse_args())
+    print('== INPUTS ==')
+    print_dict(args)
 
     # Arguments
     dir_path = 'train-in-snow'
@@ -28,11 +38,13 @@ def main():
 
     imgs_to_render = load_folder(dir_path, 6)
 
-    height, width, channels = imgs_to_render[0].shape
-
+    print('collecting shifts')
     imgs_to_render, shifts = xslit.get_shifts_and_corrected_imgs(imgs_to_render)
     images = []
-    for i in range(width // 10):
+
+    print('Computing panoramas')
+    _, width, _ = imgs_to_render[0].shape
+    for i in range(width):
         rendered_img = xslit.compute(imgs_to_render, shifts, 0, 1000, i, i)
         images.append(img2cv2(rendered_img))
 
@@ -46,6 +58,7 @@ def main():
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
     out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
 
+    print('choosing images')
     for frame in images:
         out.write(frame)  # Write out frame to video
         cv2.imshow('video', frame)
@@ -53,10 +66,11 @@ def main():
             break
 
     # Release everything if job is finished
+    print('creating video')
     out.release()
     cv2.destroyAllWindows()
 
-    print("The output video is {}".format(output))
+    print(f"The output video is {output}")
 
 
 if __name__ == '__main__':
